27b2a16d0073f755c9dbbc04d97b0d269413da20
Geonho2414 <geonhocho2414@gmail.com>
2024-04-18 14:58:09 +0900

    Update gameshop.java

--- gameshop/src/main/java/game/gameshop/Model/gameshop.java
+++ gameshop/src/main/java/game/gameshop/Model/gameshop.java
@@ -18,4 +18,64 @@ public class gameshop {
     private String image;
     @Column(name = "text", length = 65535)
     private String text;
+
+    public gameshop() {
+    }
+
+    public gameshop(long id, String title, String genre, int price, String image, String text) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.price = price;
+        this.image = image;
+        this.text = text;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
