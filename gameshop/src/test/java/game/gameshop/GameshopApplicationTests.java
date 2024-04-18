715888f6229b1367e3c1c8b1350e53364c907275
Geonho2414 <geonhocho2414@gmail.com>
2024-04-18 14:33:38 +0900

    newproj_gameshop

---
+++ gameshop/src/test/java/game/gameshop/GameshopApplicationTests.java
@@ -0,0 +1,13 @@
+package game.gameshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
