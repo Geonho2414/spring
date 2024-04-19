fd2b0814fa25e217e3928a40f58c0bedeac839e3
Geonho2414 <geonhocho2414@gmail.com>
2024-04-19 16:17:40 +0900

    2024.04.19new

---
+++ gameshop/src/main/java/gameshop/gameshop/Model/Purchase.java
@@ -0,0 +1,27 @@
+package gameshop.gameshop.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "purchase")
+public class Purchase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Gameshop game;
+    @ManyToOne
+    private User user;
+    @Column(name = "purchase_time")
+    private LocalDateTime purchaseTime;
+}
---
+++ gameshop/src/main/java/gameshop/gameshop/Repository/PurchaseRepository.java
@@ -0,0 +1,4 @@
+package gameshop.gameshop.Repository;
+
+public interface PurchaseRepository {
+}
