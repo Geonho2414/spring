f8b1f1f41e3a6ecdba0ce20d796541e808650867
Geonho2414 <geonhocho2414@gmail.com>
2024-04-19 15:57:46 +0900

    2024.04.19pm1

---
+++ purchase/src/test/java/Purchase/purchase/PurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package Purchase.purchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
