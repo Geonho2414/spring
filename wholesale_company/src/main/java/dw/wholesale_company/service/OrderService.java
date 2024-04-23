da432f652b239757e69656f1f1c4db47cddfcf3f
Geonho2414 <geonhocho2414@gmail.com>
2024-04-23 17:03:25 +0900

    Update OrderService.java

--- wholesale_company/src/main/java/dw/wholesale_company/service/OrderService.java
+++ wholesale_company/src/main/java/dw/wholesale_company/service/OrderService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -25,4 +26,8 @@ public class OrderService {
     public List<Order> getAllOrder(){
         return orderRepository.findAll();
     }
+
+
+
+    // 실습 : 주문일이 5월 1일 이후인 주문 정보 얻기
 }
