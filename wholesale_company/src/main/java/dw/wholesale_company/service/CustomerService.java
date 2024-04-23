708b8413697e3476e2e4499e601ffef93b91f092
Geonho2414 <geonhocho2414@gmail.com>
2024-04-23 14:45:35 +0900

    Create CustomerService.java

---
+++ wholesale_company/src/main/java/dw/wholesale_company/service/CustomerService.java
@@ -0,0 +1,22 @@
+package dw.wholesale_company.service;
+
+import dw.wholesale_company.model.Customer;
+import dw.wholesale_company.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer saveCustomer(Customer customer){
+        return customerRepository.save(customer);
+    }
+
+    public List<Customer> customerList
+}
