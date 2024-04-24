b94ed1f653285750306738d2f1bba0030cd32be6
Geonho2414 <geonhocho2414@gmail.com>
2024-04-24 10:33:27 +0900

    Update EmployeeService.java

--- wholesale_company/src/main/java/dw/wholesale_company/service/EmployeeService.java
+++ wholesale_company/src/main/java/dw/wholesale_company/service/EmployeeService.java
@@ -29,7 +29,9 @@ public class EmployeeService {
 
     //실습 : 사원의 직위가 '사원'인 사람들 중에서 가장 최근에 입사한 사원의 정보
     public Employee getEmployeeByHireLatest(){
-        return employeeRepository.findAll().stream().filter(e->e.getPosition().equals("사원"))
-                .sorted(Comparator.comparing(Employee::getHireDate).reversed()).findFirst().get();
+        return employeeRepository.findAll()
+                .stream().filter(e->e.getPosition().equals("사원"))
+                .sorted(Comparator.comparing(Employee::getHireDate).reversed())
+                .findFirst().get();
     }
 }
