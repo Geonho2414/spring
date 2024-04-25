e23d7fa63b8cd68630ac02b13d4b941f5554f52f
Geonho2414 <geonhocho2414@gmail.com>
2024-04-25 17:22:19 +0900

    2024.04.25pm

--- wholesale_company/src/main/java/dw/wholesale_company/service/CustomerService.java
+++ wholesale_company/src/main/java/dw/wholesale_company/service/CustomerService.java
@@ -1,11 +1,14 @@
 package dw.wholesale_company.service;
 
 import dw.wholesale_company.model.Customer;
+import dw.wholesale_company.model.Mileage;
 import dw.wholesale_company.repository.CustomerRepository;
+import dw.wholesale_company.repository.MileageRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,11 +19,14 @@ public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    MileageRepository mileageRepository;
+
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
-    public Customer saveCustomer(Customer customer){
+    public Customer saveCustomer(Customer customer) {
         return customerRepository.save(customer);
     }
 
@@ -35,7 +41,24 @@ public class CustomerService {
         for (int i = 0; i < customers.size(); i++) {
             sum = sum + customers.get(i).getMileage();
         }
-        Double avg = (double)sum / (double)customers.size();
-        return customers.stream().filter(c->c.getMileage() > avg).collect(Collectors.toList());
+        Double avg = (double) sum / (double) customers.size();
+        return customers.stream().filter(c -> c.getMileage() > avg).collect(Collectors.toList());
     }
+
+    //실습 : 마일리지 등급별로 고객수를 보이시오
+//    public List<Customer> getCustomerByMileageGrade(String grade) {
+//        List<Customer> customers = customerRepository.findAll();
+//        List<Mileage> mileages = mileageRepository.findAll();
+//        List<Mileage> KK = new ArrayList(){};
+//        int sum = 0;
+//        for (int i = 0; i < customers.size(); i ++){
+//            for (int j = 0; j < mileages.size(); j++)
+//            if (mileages.get(j).getHighLimit() > customers.get(i).getMileage()) {
+//                if (mileages.get(j).getLowLimit() > customers.get(i).getMileage()) {
+//                    sum++;
+//                }
+//            }
+//        }
+//        return mileages.stream().sorted (m -> m.getMileageGrade() = ).collect(Collectors.toList());
+//    }
 }
