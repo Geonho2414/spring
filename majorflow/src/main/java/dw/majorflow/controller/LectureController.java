d7f87620a6ab4090711a7387dae9f719ef618b4e
Geonho2414 <161662681+Geonho2414@users.noreply.github.com>
2024-06-14 09:43:20 +0900

    gg

--- majorflow/src/main/java/dw/majorflow/controller/LectureController.java
+++ majorflow/src/main/java/dw/majorflow/controller/LectureController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class LectureController {
     @GetMapping("/lectures")
     public ResponseEntity<List<Lecture>> getAllLectures() {
         return new ResponseEntity<>(lectureService.getAllLectures(), HttpStatus.OK);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Lecture> getLectureById(@PathVariable long id) {
+        return new ResponseEntity<>(LectureService.getLectureById(id),
+                HttpStatus.OK);
     }
 }
--- majorflow/src/main/java/dw/majorflow/service/LectureService.java
+++ majorflow/src/main/java/dw/majorflow/service/LectureService.java
@@ -1,12 +1,15 @@
 package dw.majorflow.service;
 
+import dw.majorflow.exception.ResourceNotFoundException;
 import dw.majorflow.model.Lecture;
 import dw.majorflow.repository.LectureRepository;
+import dw.majorflow.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LectureService {
@@ -14,7 +17,18 @@ public class LectureService {
     @Autowired
     LectureRepository lectureRepository;
 
+    UserRepository userRepository;
+
     public List<Lecture> getAllLectures() {
         return lectureRepository.findAll();
+    }
+
+    public Lecture getLectureById(long id) {
+        Optional<Lecture> optionalLecture = LectureRepository.find(id);
+        if(optionalLecture.isPresent()) {
+            return optionalLecture.get();
+        }else {
+            throw new ResourceNotFoundException("Lecture", "ID", id);
+        }
     }
 }
